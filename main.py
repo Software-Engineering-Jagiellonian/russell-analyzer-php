94e8aee0c651ff24ad3c039b78437de9fe77e659
root <kamil.ck13@gmail.com>
2021-01-03 15:52:27 +0100

    Send message to gc queue

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pika
 import json
 import subprocess
 import sys
+import time
 
 from db import Database
 import variables
@@ -58,17 +59,61 @@ def RMQ_consumer(rabbitmq_host, rabbitmq_port, queue):
                 pass
             sys.exit(0)
 
+def create_message(repo_id):
+    message = {"repo_id": repo_id, "language_id": 7}
+    return json.dumps(message)
+
+def RMQ_publisher(rabbitmq_host, rabbitmq_port, queue, repo_id):
+        try:
+            print(f"Connecting to RabbitMQ ({rabbitmq_host}:{rabbitmq_port})...")
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port))
+            channel = connection.channel()
+            print("Connected")
+
+            channel.confirm_delivery()
+
+            channel.queue_declare(queue=queue, durable=True)
+
+            while True:
+                try:
+                    channel.basic_publish(exchange='',
+                                      routing_key=queue,
+                                      properties=pika.BasicProperties(
+                                          delivery_mode=2,  # make message persistent
+                                      ),
+                                      body=bytes(create_message(repo_id), encoding='utf8'))
+                    print("Message was received by RabbitMQ")
+                    break
+                except pika.exceptions.NackError:
+                    print("Message was REJECTED by RabbitMQ (queue full?) !")
+                    time.sleep(5)
+
+        except pika.exceptions.AMQPConnectionError as exception:
+            print(f"AMQP Connection Error: {exception}")
+        except KeyboardInterrupt:
+            print(" Exiting...")
+            try:
+                connection.close()
+            except NameError:
+                pass
+            sys.exit(0)
+
 def db_get_files(repo_id):
     # Db init
     db = Database(variables.DB_DATABASE, variables.DB_USERNAME, variables.DB_PASSWORD, variables.DB_HOST, variables.DB_PORT)
 
-    execute_query = "SELECT f.file_path, f.id FROM repository_language_file as f INNER JOIN repository_language as r ON f.id = r.id WHERE repository_id ='"+repo_id+"' and language_id = 7"
+    execute_query = "SELECT f.file_path, f.id FROM repository_language_file as f INNER JOIN repository_language as r ON f.repository_language_id = r.id WHERE r.repository_id ='"+repo_id+"' AND language_id = 7 AND r.present = true AND r.analyzed = false"
 
     db.connect()
     for x in db.execute_query(execute_query):
         print(x[0])
         subprocess.run(["pdepend", "--summary-xml=metrics.xml", x[0]])
+
+    execute_query = "UPDATE repository_language SET analyzed = true where repository_id='"+repo_id+"' AND language_id = 7"
+    db.execute_query(execute_query)
     db.close()
+    RMQ_publisher(variables.RMQ_HOST, variables.RMQ_PORT, variables.QUEUE_OUT, repo_id)
+
 
 if __name__ == '__main__':
     RMQ_consumer(variables.RMQ_HOST, variables.RMQ_PORT, variables.QUEUE_IN)
