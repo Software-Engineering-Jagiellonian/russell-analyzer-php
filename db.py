c24d08f5d568cf352f313c1d4d6df9a19a8fe2b1
root <kamil.ck13@gmail.com>
2021-01-03 22:06:10 +0100

    DB modify

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import sys
 from psycopg2 import connect
 
+
 class Database:
     def __init__(self, db_name, username=None, password=None, host=None, port=None):
         self.db_name = db_name
@@ -28,6 +29,12 @@ class Database:
             self.connection.close()
         self.connection = None
 
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
     def execute_query(self, query, args=None):
         if self.connection is None or self.connection.closed:
             self.connect()
@@ -39,3 +46,28 @@ class Database:
             cursor.close()
             raise e
         return cursor
+
+    def fetchall(self, query, args=None):
+        cursor = self.execute_query(query, args)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+
+    def fetchone(self, query, args=None):
+        cursor = self.execute_query(query, args)
+        row = cursor.fetchone()
+        cursor.close()
+        return row
+
+    def insert(self, query, args):
+        cursor = self.execute_query(query, args)
+        self.commit()
+        count = cursor.rowcount
+        cursor.close()
+        return count
+
+    def update(self, query, args):
+        cursor = self.execute_query(query, args)
+        self.commit()
+        count = cursor.rowcount
+        return count
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 from db import Database
 import variables
 
+
 def RMQ_consumer_callback(ch, method, properties, body):
     ch.stop_consuming()
 
@@ -21,6 +22,7 @@ def RMQ_consumer_callback(ch, method, properties, body):
         print("Message received is not a JSON")
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 def RMQ_consumer(rabbitmq_host, rabbitmq_port, queue):
     while True:
@@ -54,54 +56,55 @@ def create_message(repo_id):
     return json.dumps(message)
 
 def RMQ_publisher(rabbitmq_host, rabbitmq_port, queue, repo_id):
-        try:
-            print(f"{variables.QUEUE_OUT} - Connecting to RabbitMQ ({rabbitmq_host}:{rabbitmq_port})...")
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port))
-            channel = connection.channel()
-            print(f"{variables.QUEUE_OUT} - Connected")
+    try:
+        print(f"{variables.QUEUE_OUT} - Connecting to RabbitMQ ({rabbitmq_host}:{rabbitmq_port})...")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port))
+        channel = connection.channel()
+        print(f"{variables.QUEUE_OUT} - Connected")
 
-            channel.confirm_delivery()
+        channel.confirm_delivery()
 
-            channel.queue_declare(queue=queue, durable=True)
+        channel.queue_declare(queue=queue, durable=True)
 
-            while True:
-                try:
-                    channel.basic_publish(exchange='',
+        while True:
+            try:
+                channel.basic_publish(exchange='',
                                       routing_key=queue,
                                       properties=pika.BasicProperties(
                                           delivery_mode=2,  # make message persistent
                                       ),
                                       body=bytes(create_message(repo_id), encoding='utf8'))
-                    print(f"{variables.QUEUE_OUT} - Message was received by RabbitMQ")
-                    break
-                except pika.exceptions.NackError:
-                    print(f"{variables.QUEUE_OUT} - Message was REJECTED by RabbitMQ (queue {variables.QUEUE_OUT} full?) !")
-                    time.sleep(5)
+                print(f"{variables.QUEUE_OUT} - Message was received by RabbitMQ")
+                break
+            except pika.exceptions.NackError:
+                print(f"{variables.QUEUE_OUT} - Message was REJECTED by RabbitMQ (queue {variables.QUEUE_OUT} full?) !")
+                time.sleep(5)
 
-        except pika.exceptions.AMQPConnectionError as exception:
-            print(f"{variables.QUEUE_OUT} - AMQP Connection Error: {exception}")
-        except KeyboardInterrupt:
-            print(" Exiting...")
-            try:
-                connection.close()
-            except NameError:
-                pass
-            sys.exit(0)
+    except pika.exceptions.AMQPConnectionError as exception:
+        print(f"{variables.QUEUE_OUT} - AMQP Connection Error: {exception}")
+    except KeyboardInterrupt:
+        print(" Exiting...")
+        try:
+            connection.close()
+        except NameError:
+            pass
+        sys.exit(0)
 
 def calculate_metrics(repo_id):
-    db = Database(variables.DB_DATABASE, variables.DB_USERNAME, variables.DB_PASSWORD, variables.DB_HOST, variables.DB_PORT)
+    db = Database(variables.DB_DATABASE, variables.DB_USERNAME, variables.DB_PASSWORD, variables.DB_HOST,
+                  variables.DB_PORT)
 
-    execute_query = "SELECT f.file_path, f.id FROM repository_language_file as f INNER JOIN repository_language as r ON f.repository_language_id = r.id WHERE r.repository_id ='"+repo_id+"' AND language_id = "+variables.LANGUAGE_ID+" AND r.present = true AND r.analyzed = false"
+    execute_query = "SELECT f.file_path, f.id FROM repository_language_file as f INNER JOIN repository_language as r ON f.repository_language_id = r.id WHERE r.repository_id ='" + repo_id + "' AND language_id = " + variables.LANGUAGE_ID + " AND r.present = true AND r.analyzed = false"
 
     db.connect()
-    for x in db.execute_query(execute_query):
+    for x in db.insert(execute_query, ""):
         print(x[0])
         subprocess.run(["pdepend", "--summary-xml=metrics.xml", x[0]])
 
-    execute_query = "UPDATE repository_language SET analyzed = true where repository_id='"+repo_id+"' AND language_id = "+variables.LANGUAGE_ID
-    db.execute_query(execute_query)
+    execute_query = "UPDATE repository_language SET analyzed = true WHERE repository_id='" + repo_id + "' AND language_id = " + variables.LANGUAGE_ID
+    db.update(execute_query, "")
     db.close()
-    RMQ_publisher(variables.RMQ_HOST, variables.RMQ_PORT, variables.QUEUE_OUT, repo_id) #send confirmation to gc
+    RMQ_publisher(variables.RMQ_HOST, variables.RMQ_PORT, variables.QUEUE_OUT, repo_id)  # send confirmation to gc
 
 if __name__ == '__main__':
     RMQ_consumer(variables.RMQ_HOST, variables.RMQ_PORT, variables.QUEUE_IN)
